05d8bb23623a0626c00cb4675eee9ab72292dd03
Csabbah <csabbah@ryerson.ca>
2022-05-14 16:56:02 -0400

    Included base setup for template engine as well as imported all other required app.use() & object declarations in server.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,10 @@
 // The main page with all the main templates
-
 const router = require('express').Router();
 
-// get all posts for homepage
+// In this path, the main.handlebars template renders always and inside the {{{body}}} section....
+// We render the homepage.handlebars template (which is a simple <h1>Hi Son</h1>)
 router.get('/', (req, res) => {
-  res.json('hi');
+  res.render('homepage');
 });
 
 module.exports = router;
