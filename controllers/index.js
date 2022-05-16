7e7f40a04811e92b21911409f40d45ab51121dbf
Csabbah <csabbah@ryerson.ca>
2022-05-16 12:32:42 -0400

    Updated main and homepage templates to display required nav links and hardcoded post elements (including CSS styling)

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
+const loginRoutes = require('./login-routes.js');
 
 router.use('/', homeRoutes);
+router.use('/login', loginRoutes);
 
 module.exports = router;
