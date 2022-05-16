3de8670f97396739208cdb80ed076cfaf445e89b
Csabbah <csabbah@ryerson.ca>
2022-05-16 15:55:37 -0400

    Finalized dashboard template styling and layout

---
+++ controllers/dashboard.js
@@ -0,0 +1,8 @@
+// Dashboard page
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res.render('dashboard');
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 const loginRoutes = require('./login-routes.js');
+const dashboardRoutes = require('./dashboard.js');
 
 router.use('/', homeRoutes);
 router.use('/login', loginRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
