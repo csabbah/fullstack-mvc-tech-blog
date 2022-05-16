6abacc79111d996bf8b09ed475360bb53235ebc5
Csabbah <csabbah@ryerson.ca>
2022-05-16 18:26:41 -0400

    homepage template now returns active posts from the database

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,20 +6,18 @@ const { Post, User } = require('../models');
 // In this path, the main.handlebars template renders always and inside the {{{body}}} section....
 // We render the homepage.handlebars template (which is a simple <h1>Hi Son</h1>)
 router.get('/', (req, res) => {
-  res.render('homepage');
-
-  // Post.findAll({
-  //   attributes: ['id', 'post_url', 'title', 'created_at', 'user_id'],
-  // })
-  //   .then((dbPostData) => {
-  //     // serialize the data
-  //     const posts = dbPostData.map((post) => post.get({ plain: true }));
-  //     res.render('homepage', { posts });
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   });
+  Post.findAll({
+    attributes: ['id', 'post_url', 'title', 'created_at', 'user_id'],
+  })
+    .then((dbPostData) => {
+      // serialize the data
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render('homepage', { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
