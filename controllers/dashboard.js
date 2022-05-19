a0d4dc083d5d59d7be0ca950c08c6a3b78a1cde7
Csabbah <csabbah@ryerson.ca>
2022-05-19 14:39:54 -0400

    The application will now display which account the user is logged in as & only when logged in can the user access the dashboard page

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 
+const withAuth = require('../utils/auth');
+
 // Return all posts associated with the user
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
   // Add a new route here that returns all posts associated with user, you can easily extract this via 'req.session.user_id'
   // Return all users active posts in the data base
   Post.findAll({
@@ -40,14 +42,19 @@ router.get('/', (req, res) => {
           posts.push({ postId, title, description, date });
         });
       }
-      // // For each comment, push it to the array inside our object
+      // UPDATE THIS LINE OF BLOCK
+      // For each comment, push it to the array inside our object
       // for (let i = 0; i < dbPostData.dataValues.comments.length; i++) {
       //   let username = dbPostData.dataValues.comments[i].user.username;
       //   let commentText = dbPostData.dataValues.comments[i].comment_text;
       //   post.comments.push({ user: username, text: commentText });
       // }
 
-      res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
+      res.render('dashboard', {
+        posts,
+        loggedIn: req.session.loggedIn,
+        username: req.session.username,
+      });
     })
     .catch((err) => {
       console.log(err);
